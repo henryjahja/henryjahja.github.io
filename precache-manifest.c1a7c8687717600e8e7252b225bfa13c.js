700f2c1e96766ca61bfb3131ead6c79888057077
Henry Jahja <henryjahja@users.noreply.github.com>
2023-06-18 16:21:45 -0700

    Updates

---
+++ precache-manifest.c1a7c8687717600e8e7252b225bfa13c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb81e459d0bba0356117487814d8593c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d34d419c71fa795dae9e",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "9d66f235aeac6bce36bf",
+    "url": "./static/css/main.95798c22.chunk.css"
+  },
+  {
+    "revision": "d34d419c71fa795dae9e",
+    "url": "./static/js/2.2d055549.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.2d055549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d66f235aeac6bce36bf",
+    "url": "./static/js/main.fe610eed.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
